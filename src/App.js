b7a420bdfc68b692eaf58433989c367aa899e02e
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-07-16 16:54:47 -0400

    established localstorage for todos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
+const { v4: uuidv4 } = require('uuid');
+
+const LOCAL_STORAGE_KEY = "todoApp.todos"
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: "Todo1", complete: true }])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === "") return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     // The below fragment element allows you to return multiple elements in one rfc
     <>
       < TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to complete; Good Job!</div>
     </>
