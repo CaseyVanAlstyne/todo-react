e62456ad7412da1c53b7eccc4c57c75fcda11b33
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-07-16 15:53:04 -0400

    added text input and a few buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    < TodoList />
+    // The below fragment element allows you to return multiple elements in one rfc
+    <>
+      < TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to complete; Good Job!</div>
+    </>
   )
 }
 
