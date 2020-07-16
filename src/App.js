999572dd52b0d057a1be686f2830070b60fcae72
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-07-16 16:24:58 -0400

    added hook for mapping todo from array. Also added checkbox.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 1, name: "Todo1", complete: true }])
   return (
     // The below fragment element allows you to return multiple elements in one rfc
     <>
-      < TodoList />
+      < TodoList todos={todos} />
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed Todos</button>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Todo from "./Todo";
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
     return (
-        <div>
-            "Hello World"
-        </div>
+        todos.map(todo => {
+            return <Todo key={todo.id} todo={todo} />
+        })
     )
 }
